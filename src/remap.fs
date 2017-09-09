ebd1efff27d03b6d126305106dbd15a1fbbc0c3f
fluffy <fluffy@beesbuzz.biz>
2017-09-08 22:17:56 -0700

    Fix(?) the annoying roundoff error in remap.fs

--- src/remap.fs
+++ src/remap.fs
@@ -8,7 +8,7 @@ uniform Image referred;
 // returns premultiplied alpha
 vec4 effect(vec4 color, Image texture, vec2 texture_coords, vec2 screen_coords) {
     vec4 here = Texel(texture, texture_coords);
-    vec4 there = Texel(referred, here.rg);
+    vec4 there = Texel(referred, here.rg*255.0/256.0);
 
     return vec4(color.rgb * there.rgb, 1.0) * color.a * here.a;
 }

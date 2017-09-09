a1b38bb597b43d475564cd47635a54bb47ef4ed0
fluffy <fluffy@beesbuzz.biz>
2017-09-08 21:58:45 -0700

    More silly unrolling

--- src/reduce.fs
+++ src/reduce.fs
@@ -3,31 +3,28 @@ uniform vec2 size; // texture size, in texels
 vec4 effect(vec4 color, Image texture, vec2 texture_coords, vec2 screen_coords) {
     vec2 ss = vec2(1.0, 1.0)/size;
 
-    vec4 val[5] = vec4[5](
-        Texel(texture, texture_coords),
-        Texel(texture, texture_coords + vec2(ss.x,0)),
-        Texel(texture, texture_coords - vec2(ss.x,0)),
-        Texel(texture, texture_coords + vec2(0,ss.y)),
-        Texel(texture, texture_coords - vec2(0,ss.y))
-    );
+    // unrolls brought to you by wanting to support GLSL ES 1.0
+    vec4 val0 = Texel(texture, texture_coords);
+    vec4 val1 = Texel(texture, texture_coords + vec2(ss.x,0));
+    vec4 val2 = Texel(texture, texture_coords - vec2(ss.x,0));
+    vec4 val3 = Texel(texture, texture_coords + vec2(0,ss.y));
+    vec4 val4 = Texel(texture, texture_coords - vec2(0,ss.y));
 
-    // silly unrolling, necessary for older GPUs
-    int count[4] = int[4](
-        (val[0] == val[1] ? 1 : 0)
-        + (val[0] == val[2] ? 1 : 0)
-        + (val[0] == val[3] ? 1 : 0)
-        + (val[0] == val[4] ? 1 : 0),
-        (val[1] == val[2] ? 1 : 0)
-        + (val[1] == val[3] ? 1 : 0)
-        + (val[1] == val[4] ? 1 : 0),
-        (val[2] == val[3] ? 1 : 0)
-        + (val[2] == val[4] ? 1 : 0),
-        (val[3] == val[4] ? 1 : 0));
+    int count0 = (val0 == val1 ? 1 : 0)
+        + (val0 == val2 ? 1 : 0)
+        + (val0 == val3 ? 1 : 0)
+        + (val0 == val4 ? 1 : 0);
+    int count1 = (val1 == val2 ? 1 : 0)
+        + (val1 == val3 ? 1 : 0)
+        + (val1 == val4 ? 1 : 0);
+    int count2 = (val2 == val3 ? 1 : 0)
+        + (val2 == val4 ? 1 : 0);
+    int count3 = (val3 == val4 ? 1 : 0);
 
-    int maxc = 0;
-    maxc = (count[1] > count[maxc]) ? 1 : maxc;
-    maxc = (count[2] > count[maxc]) ? 2 : maxc;
-    maxc = (count[3] > count[maxc]) ? 3 : maxc;
+    vec4 rc = val0;
+    rc = count1 > count0 ? val1 : rc;
+    rc = count2 > count1 && count2 > count0 ? val2 : rc;
+    rc = count3 > count2 && count3 > count1 && count3 > count0 ? val3 : rc;
 
-    return color*val[maxc];
+    return color*rc;
 }

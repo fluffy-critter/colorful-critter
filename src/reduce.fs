54df0671d84847dc78156276e3f34670fad129da
fluffy <fluffy@beesbuzz.biz>
2017-09-08 21:48:48 -0700

    More unrolling in the shader

--- src/reduce.fs
+++ src/reduce.fs
@@ -11,18 +11,23 @@ vec4 effect(vec4 color, Image texture, vec2 texture_coords, vec2 screen_coords) 
         Texel(texture, texture_coords - vec2(0,ss.y))
     );
 
-    int count[5] = int[5](0,0,0,0,0);
-
-    for (int i = 0; i < 4; i++) {
-        for (int j = i + 1; j < 5; j++) {
-            count[i] += (val[i] == val[j]) ? 1 : 0;
-        }
-    }
+    // silly unrolling, necessary for older GPUs
+    int count[4] = int[4](
+        (val[0] == val[1] ? 1 : 0)
+        + (val[0] == val[2] ? 1 : 0)
+        + (val[0] == val[3] ? 1 : 0)
+        + (val[0] == val[4] ? 1 : 0),
+        (val[1] == val[2] ? 1 : 0)
+        + (val[1] == val[3] ? 1 : 0)
+        + (val[1] == val[4] ? 1 : 0),
+        (val[2] == val[3] ? 1 : 0)
+        + (val[2] == val[4] ? 1 : 0),
+        (val[3] == val[4] ? 1 : 0));
 
     int maxc = 0;
-    for (int i = 1; i < 5; i++) {
-        maxc = (count[i] > count[maxc]) ? i : maxc;
-    }
+    maxc = (count[1] > count[maxc]) ? 1 : maxc;
+    maxc = (count[2] > count[maxc]) ? 2 : maxc;
+    maxc = (count[3] > count[maxc]) ? 3 : maxc;
 
     return color*val[maxc];
 }

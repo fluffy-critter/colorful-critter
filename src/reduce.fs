edf725694d6ee30bdf808a980dfa6ab9001ddfb9
fluffy <fluffy@beesbuzz.biz>
2017-09-08 22:13:46 -0700

    Maximum unrollge

--- src/reduce.fs
+++ src/reduce.fs
@@ -1,7 +1,7 @@
 uniform vec2 size; // texture size, in texels
 
 vec4 effect(vec4 color, Image texture, vec2 texture_coords, vec2 screen_coords) {
-    vec2 ss = vec2(1.0, 1.0)/size;
+    vec2 ss = vec2(1.5, 1.5)/size;
 
     // unrolls brought to you by wanting to support GLSL ES 1.0
     vec4 val0 = Texel(texture, texture_coords);
@@ -21,10 +21,7 @@ vec4 effect(vec4 color, Image texture, vec2 texture_coords, vec2 screen_coords) 
         + (val2 == val4 ? 1 : 0);
     int count3 = (val3 == val4 ? 1 : 0);
 
-    vec4 rc = val0;
-    rc = count1 > count0 ? val1 : rc;
-    rc = count2 > count1 && count2 > count0 ? val2 : rc;
-    rc = count3 > count2 && count3 > count1 && count3 > count0 ? val3 : rc;
-
-    return color*rc;
+    return color*(count3 > count2 && count3 > count1 && count3 > count0 ? val3 :
+        (count2 > count1 && count2 > count0 ? val2 :
+            (count1 > count0 ? val1 : val0)));
 }
